3e259eb89b9675c75488de17b539f6aa0ce8e5ff
mitesh53 <miteshrathore53@gmail.com>
2023-02-18 14:44:20 +0530

    Initial Commit

---
+++ DoubleArray/DoubleArray.h
@@ -0,0 +1,18 @@
+//
+//  DoubleArray.h
+//  DoubleArray
+//
+//  Created by Mitesh Rathore on 18/02/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DoubleArray.
+FOUNDATION_EXPORT double DoubleArrayVersionNumber;
+
+//! Project version string for DoubleArray.
+FOUNDATION_EXPORT const unsigned char DoubleArrayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DoubleArray/PublicHeader.h>
+
+
